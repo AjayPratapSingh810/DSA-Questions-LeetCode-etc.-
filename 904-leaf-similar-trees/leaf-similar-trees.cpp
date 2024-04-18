1c124276cd3333ff27b4ca2383c58f12bde9ef80
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-04-18 19:27:56 +0530

    Time: 7 ms (16.82%) | Memory: 14.2 MB (59.11%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -12,21 +12,20 @@
 class Solution {
 public:
     void solve(TreeNode* root,vector<int> &arr){
-        if(root == NULL){
-            return;
-        }
         if(root->left == NULL && root->right == NULL){
             arr.push_back(root->val);
             return;
         }
-        solve(root->left,arr);
-        solve(root->right,arr);
-        return;
+        if(root->left){
+            solve(root->left,arr);
+        }
+        if(root->right){
+            solve(root->right,arr);
+        }
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
         vector<int> arr1;
         vector<int> arr2;
-
         solve(root1,arr1);
         solve(root2,arr2);
         if(arr1 == arr2){
