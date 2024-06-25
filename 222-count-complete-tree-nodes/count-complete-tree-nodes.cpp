9ff5d9c813211a3401c996aaa70313ad2d3ddb53
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-06-25 08:33:03 +0530

    Time: 15 ms (96.66%) | Memory: 29 MB (78.92%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,17 +11,31 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root,int &count){
+    int solve(TreeNode* root){
         if(root == NULL){
-            return;
+            return 0;
         }
-        solve(root->left,count);
-        count++;
-        solve(root->right,count);
+        int lc = 0;
+        int rc = 0;
+        TreeNode* curr = root;
+
+        while(curr != NULL){
+            curr = curr->left;
+            lc++;
+        }
+        curr = root;
+        while(curr != NULL){
+            curr = curr->right;
+            rc++;
+        }
+
+        if(lc == rc){
+            return pow(2,lc)-1;
+        }
+        return (1+solve(root->left)+solve(root->right));
     }
     int countNodes(TreeNode* root) {
         int count = 0;
-        solve(root,count);
-        return count;
+        return solve(root);
     }
 };
