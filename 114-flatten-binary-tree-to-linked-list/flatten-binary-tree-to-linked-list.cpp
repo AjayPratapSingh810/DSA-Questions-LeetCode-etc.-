6ad6f18ae19a6c94ae07720c5d9b0ec0979332c8
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-07-03 07:32:46 +0530

    Time: 3 ms (77.32%) | Memory: 15.5 MB (86.72%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -6,37 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* solve(TreeNode* root) {
-        if (root == NULL) {
-            return NULL;
-        }
-        TreeNode* leftSide = solve(root->left);
-        TreeNode* rightSide = solve(root->right);
-
-        if (leftSide != NULL && rightSide != NULL) {
-            root->left = NULL;
-            root->right = leftSide;
-            TreeNode* curr = root->right;
-            while (curr->right != NULL) {
-                curr = curr->right;
-            }
-            curr->right = rightSide;
-        } else if (leftSide != NULL && rightSide == NULL) {
-            root->left = NULL;
-            root->right = leftSide;
-        } else if (leftSide == NULL && rightSide != NULL) {
-            root->left = NULL;
-            root->right = rightSide;
-        }
-        return root;
-    }
     void flatten(TreeNode* root) {
-        solve(root);
+        TreeNode* curr = root;
+        while(curr){
+            TreeNode* prev = NULL;
+            if(curr->left){
+                prev = curr->left;
+                while(prev->right != NULL){
+                    prev = prev->right;
+                }
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
+        }
     }
 };
