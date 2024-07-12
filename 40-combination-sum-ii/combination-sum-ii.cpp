e638a1ffa967ddadf4b7272c7b7b162c28194dfb
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-07-12 11:03:20 +0530

    Time: 28 ms (8.91%) | Memory: 19.2 MB (8.61%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(int index,vector<vector<int> > &ans,vector<int> &arr,vector<int>& candidates,int target,int sum){
+    void solve(vector<int>& candidates,int target,vector<vector<int>> &ans,vector<int> arr,int sum,int index){
         if(sum > target){
             return;
         }
@@ -8,22 +8,24 @@ public:
             ans.push_back(arr);
             return;
         }
-        for(int i = index;i<candidates.size();i++){
-            if(i>index && candidates[i]==candidates[i-1])
+        for(int i=index;i<candidates.size();i++){
+            if(i > index && candidates[i] == candidates[i-1]){
                 continue;
-            sum = sum + candidates[i];
+            }
             arr.push_back(candidates[i]);
-            solve(i+1,ans,arr,candidates,target,sum);
+            sum = sum+candidates[i];
+            solve(candidates,target,ans,arr,sum,i+1);
             arr.pop_back();
             sum = sum-candidates[i];
         }
-
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int> > ans;
+        vector<vector<int>> ans;
         vector<int> arr;
-        solve(0,ans,arr,candidates,target,0);
+        int sum = 0;
+        int index = 0;
+        sort(candidates.begin(),candidates.end());
+        solve(candidates,target,ans,arr,sum,index);
         return ans;
     }
 };
