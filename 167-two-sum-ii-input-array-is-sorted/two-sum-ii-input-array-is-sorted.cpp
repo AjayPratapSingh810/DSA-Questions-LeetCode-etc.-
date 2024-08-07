d01ad1dfe9126a1750db59fc8bfa4dbecc752b54
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-08-07 07:45:36 +0530

    Time: 3 ms (98.20%) | Memory: 18.3 MB (29.24%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -4,17 +4,16 @@ public:
         int s = 0;
         int e = numbers.size()-1;
         vector<int> ans;
+
         while(s<e){
-            int a = numbers[s];
-            int b = numbers[e];
-            if(a+b == target){
+            if(numbers[s]+numbers[e] == target){
                 ans.push_back(s+1);
                 ans.push_back(e+1);
                 return ans;
-            }else if(a+b < target){
-                s++;
+            }else if(numbers[s] + numbers[e] < target){
+                s = s+1;
             }else{
-                e--;
+                e = e-1;
             }
         }
         return ans;
