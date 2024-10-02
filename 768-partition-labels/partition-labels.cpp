f9f4f2cbe045f6fea6a4f2183904686fd1be9217
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-10-02 09:57:42 +0530

    Time: 6 ms (28.78%) | Memory: 9.4 MB (37.91%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int lastIndex = INT_MIN;
-        vector<string> strings;
-        int i = 0;
-        string temp = "";
-        while(i<s.length()){
-            char a = s[i];
-            bool insertion = true;
-            
-            for(int j=s.length()-1;j>=i;j--){
-                if(s[j] == a){
-                    temp.push_back(s[i]);
-                    i++;
-                    if(lastIndex < j){
-                        lastIndex = j;
-                    }
-                    insertion = false;
-                    break;
-                }
-            }
-            if(lastIndex < i){
-                lastIndex= INT_MIN;
-                strings.push_back(temp);
-                temp = "";
-            }
-            if(insertion){
-                temp.push_back(s[i]);
-                    i++;
-            }
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.length();i++){
+            mp[s[i]] = i;
         }
 
+        int i = 0;
+        int lastIndex = mp[s[0]];
         vector<int> ans;
-        for(int i=0;i<strings.size();i++){
-            string a = strings[i];
-            ans.push_back(a.length());
+        while(i < s.length() && lastIndex < s.length()){
+            int j = lastIndex-1;
+            bool happn = true;
+            while(j > i){
+                if(mp[s[j]] > lastIndex){
+                    lastIndex = mp[s[j]];
+                    happn = false;
+                }
+                j--;
+            }
+            if(happn){
+                ans.push_back(lastIndex-i+1);
+                i = lastIndex+1;
+                if(i < s.length()){
+                    lastIndex = mp[s[i]];
+                }
+            }
         }
         return ans;
     }
