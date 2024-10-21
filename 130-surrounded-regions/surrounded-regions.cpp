c8769b2ddcc46ad5936e2540719bb4543da4c36f
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-10-21 21:33:59 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (40.18%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,66 +1,51 @@
 class Solution {
 public:
-void dfsVisited(vector<vector<char>>& board,vector<vector<bool> > &visited,int x,int y,int rows,int cols){
-    visited[x][y] = true;
-    // up
-    if(x > 0){
-        if(!visited[x-1][y] && board[x-1][y] == 'O'){
-            dfsVisited(board,visited,x-1,y,rows,cols);
-        }
-    }
-    if(x < rows-1){
-        if(!visited[x+1][y] && board[x+1][y] == 'O'){
-            dfsVisited(board,visited,x+1,y,rows,cols);
-        }
-    }
-    if(y > 0){
-        if(!visited[x][y-1] && board[x][y-1] == 'O'){
-            dfsVisited(board,visited,x,y-1,rows,cols);
-        }
-    }
-    if(y < cols-1){
-        if(!visited[x][y+1] && board[x][y+1] == 'O'){
-            dfsVisited(board,visited,x,y+1,rows,cols);
-        }
-    }
-}
     void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
-         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        //  1st row
-        for(int i=0;i<n;i++){
-            if(!visited[0][i] && board[0][i] == 'O'){
-                dfsVisited(board,visited,0,i,m,n);
-            }
-        }
-        // 1st col
-        for(int i=0;i<m;i++){
-            if(!visited[i][0] && board[i][0] == 'O'){
-                dfsVisited(board,visited,i,0,m,n);
-            }
-        }
-        // last col
-        for(int i=0;i<m;i++){
-            if(!visited[i][n-1] && board[i][n-1] == 'O'){
-                dfsVisited(board,visited,i,n-1,m,n);
-            }
-        }
-        // last row
-        for(int i=0;i<n;i++){
-            if(!visited[m-1][i] && board[m-1][i] == 'O'){
-                dfsVisited(board,visited,m-1,i,m,n);
-            }
-        }
+        int rows = board.size();
+        int cols = board[0].size();
+        vector<vector<bool> > visited(rows,vector<bool> (cols,false));
+        vector<vector<char> > ans(rows,vector<char> (cols,'X'));
 
-        for(int i =0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(visited[i][j] == true){
-                    board[i][j] = 'O';
-                }else{
-                    board[i][j] = 'X';
+        queue<pair<int,int> > q;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if((i == 0 || j == 0 || i == rows-1 || j == cols-1) && board[i][j] == 'O'){
+                    visited[i][j] = true;
+                    q.push(make_pair(i,j));
+                    ans[i][j] = 'O';
                 }
             }
         }
+
+        while(!q.empty()){
+            pair<int,int> p = q.front();
+            q.pop();
+            int i= p.first;
+            int j= p.second;
+
+            if(i > 0 && visited[i-1][j] == false && board[i-1][j] == 'O'){
+                q.push(make_pair(i-1,j));
+                visited[i-1][j] = true; 
+                ans[i-1][j] = 'O';
+            }
+            if(j < cols-1 && visited[i][j+1] == false && board[i][j+1] == 'O'){
+                q.push(make_pair(i,j+1));
+                visited[i][j+1] = true;
+                ans[i][j+1] = 'O'; 
+            }
+            if(i < rows-1 && visited[i+1][j] == false && board[i+1][j] == 'O'){
+                q.push(make_pair(i+1,j));
+                visited[i+1][j] = true;
+                ans[i+1][j] = 'O'; 
+            }
+            if(j > 0 && visited[i][j-1] == false && board[i][j-1] == 'O'){
+                q.push(make_pair(i,j-1));
+                visited[i][j-1] = true;
+                ans[i][j-1] = 'O';
+            }
+
+        }
+        board =  ans;
     }
 };
