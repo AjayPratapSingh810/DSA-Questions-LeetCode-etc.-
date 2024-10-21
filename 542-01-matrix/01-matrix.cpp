bdda67f66fa88e6043b335670dee2d182313eb02
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-10-21 21:04:55 +0530

    Time: 27 ms (98.39%) | Memory: 37.4 MB (56.57%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(mat[i][j] == 1)
-                mat[i][j] = INT_MAX;
-            }
-        }
-
-        // First pass: top-to-bottom, left-to-right
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] != 0) {
-                    if (i > 0 && mat[i-1][j] != INT_MAX) {
-                        mat[i][j] = min(mat[i][j], mat[i-1][j] + 1);
-                    }
-                    if (j > 0 && mat[i][j-1] != INT_MAX) {
-                        mat[i][j] = min(mat[i][j], mat[i][j-1] + 1);
-                    }
+        queue<pair<int,pair<int,int> > > q;
+        int size = mat.size();
+        int size2 = mat[0].size();
+        vector<vector<int> > ans(size,vector<int> (size2,0));
+        vector<vector<bool> > visited(size,vector<bool> (size2,false));
+        for(int i=0;i<mat.size();i++){
+            for(int j= 0;j<mat[i].size();j++){
+                if(mat[i][j] == 0){
+                    q.push(make_pair(0,make_pair(i,j)));
+                    visited[i][j] = true;
                 }
             }
         }
 
-        // Second pass: bottom-to-top, right-to-left
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (mat[i][j] != 0) {
-                    if (i < m - 1 && mat[i+1][j] != INT_MAX) {
-                        mat[i][j] = min(mat[i][j], mat[i+1][j] + 1);
-                    }
-                    if (j < n - 1 && mat[i][j+1] != INT_MAX) {
-                        mat[i][j] = min(mat[i][j], mat[i][j+1] + 1);
-                    }
-                }
+        while(!q.empty()){
+            pair<int,pair<int,int> > p = q.front();
+            q.pop();
+            int x = p.first;
+            int i= p.second.first;
+            int j= p.second.second;
+
+            if(i>0 && !visited[i-1][j]){
+                visited[i-1][j] = true;
+                ans[i-1][j] = x+1;
+                q.push(make_pair(x+1,make_pair(i-1,j)));
+            }
+            if(j<mat[i].size()-1 && !visited[i][j+1]){
+                visited[i][j+1] = true;
+                ans[i][j+1] = x+1;
+                q.push(make_pair(x+1,make_pair(i,j+1)));
+            } 
+            if(i<mat.size()-1 && !visited[i+1][j]){
+                visited[i+1][j] = true;
+                ans[i+1][j] = x+1;
+                q.push(make_pair(x+1,make_pair(i+1,j)));
+            }
+            if(j>0 && !visited[i][j-1]){
+                visited[i][j-1] = true;
+                ans[i][j-1] = x+1;
+                q.push(make_pair(x+1,make_pair(i,j-1)));
             }
         }
-        return mat;
+        return ans;
     }
 };
