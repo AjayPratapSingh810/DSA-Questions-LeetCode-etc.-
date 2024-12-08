6bd31a453fece9436c5c21e48057487b27996914
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-12-08 13:35:51 +0530

    Time: 21 ms (6.26%) | Memory: 32 MB (5.01%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
-    void func(string s,int lc,int rc,vector<string> &ans){
-        if(lc == 0 && rc == 0){
+    void solve(int n , vector<string> &ans,string s,int l,int r){
+        if(l == n && r == n){
             ans.push_back(s);
             return;
         }
-        if(lc == rc){
-            func(s+'(',lc-1,rc,ans);
-        }else if(lc < rc){
-            if(rc > 0){
-                func(s+')',lc,rc-1,ans);
-            }
-            if(lc > 0){
-                func(s+'(',lc-1,rc,ans);
+        if(l < n){
+            s.push_back('(');
+            solve(n,ans,s,l+1,r);
+            s.pop_back();
+        }
+        if(!s.empty() && r < n){
+            s.push_back(')');
+            solve(n,ans,s,l,r+1);
+            s.pop_back();
+        }
+        return;
+
+    }
+    bool valid(string s){
+        stack<char> st;
+        for(int i = 0;i<s.length();i++){
+            if(s[i] == '('){
+                st.push('(');
+            }else if(s[i] == ')'){
+                if(st.empty()){
+                    return false;
+                }
+                st.pop();
             }
         }
+        if(!st.empty()){
+            return false;
+        }
+        return true;
     }
     vector<string> generateParenthesis(int n) {
-        int lc = n;
-        int rc = n;
         vector<string> ans;
-        func("",lc,rc,ans);
-        return ans;
+        solve(n,ans,"",0,0);
+        vector<string> arr;
+        for(int i = 0;i<ans.size();i++){
+            bool check = valid(ans[i]);
+            if(check){
+                arr.push_back(ans[i]);
+            }
+        }
+        return arr;
     }
 };
