f3cb8213ecead52541d727ab09b820a903feed38
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-12-08 13:44:41 +0530

    Time: 5 ms (21.86%) | Memory: 15.6 MB (33.12%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,50 +1,69 @@
 class Solution {
 public:
-    void solve(int n , vector<string> &ans,string s,int l,int r){
+    // void solve(int n , vector<string> &ans,string s,int l,int r){
+    //     if(l == n && r == n){
+    //         ans.push_back(s);
+    //         return;
+    //     }
+    //     if(l < n){
+    //         s.push_back('(');
+    //         solve(n,ans,s,l+1,r);
+    //         s.pop_back();
+    //     }
+    //     if(!s.empty() && r < n){
+    //         s.push_back(')');
+    //         solve(n,ans,s,l,r+1);
+    //         s.pop_back();
+    //     }
+    //     return;
+
+    // }
+    // bool valid(string s){
+    //     stack<char> st;
+    //     for(int i = 0;i<s.length();i++){
+    //         if(s[i] == '('){
+    //             st.push('(');
+    //         }else if(s[i] == ')'){
+    //             if(st.empty()){
+    //                 return false;
+    //             }
+    //             st.pop();
+    //         }
+    //     }
+    //     if(!st.empty()){
+    //         return false;
+    //     }
+    //     return true;
+    // }
+        void solve(int l,int r,int n,vector<string> &ans,string s){
         if(l == n && r == n){
             ans.push_back(s);
             return;
-        }
-        if(l < n){
-            s.push_back('(');
-            solve(n,ans,s,l+1,r);
-            s.pop_back();
-        }
-        if(!s.empty() && r < n){
-            s.push_back(')');
-            solve(n,ans,s,l,r+1);
-            s.pop_back();
-        }
-        return;
-
-    }
-    bool valid(string s){
-        stack<char> st;
-        for(int i = 0;i<s.length();i++){
-            if(s[i] == '('){
-                st.push('(');
-            }else if(s[i] == ')'){
-                if(st.empty()){
-                    return false;
-                }
-                st.pop();
+        }else if(l == r){
+            solve(l+1,r,n,ans,s+'(');
+        }else if(l > r){
+            if( r < n ){
+                solve(l,r+1,n,ans,s+')');
+            }
+            if(l < n){
+                solve(l+1,r,n,ans,s+'(');
             }
         }
-        if(!st.empty()){
-            return false;
-        }
-        return true;
     }
     vector<string> generateParenthesis(int n) {
+        // vector<string> ans;
+        // solve(n,ans,"",0,0);
+        // vector<string> arr;
+        // for(int i = 0;i<ans.size();i++){
+        //     bool check = valid(ans[i]);
+        //     if(check){
+        //         arr.push_back(ans[i]);
+        //     }
+        // }
+        // return arr;
+
         vector<string> ans;
-        solve(n,ans,"",0,0);
-        vector<string> arr;
-        for(int i = 0;i<ans.size();i++){
-            bool check = valid(ans[i]);
-            if(check){
-                arr.push_back(ans[i]);
-            }
-        }
-        return arr;
+        solve(0,0,n,ans,"");
+        return ans;
     }
 };
