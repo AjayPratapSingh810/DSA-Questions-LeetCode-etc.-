b96ad5e4d30f641d97d0d6e164c3c6b4ed990866
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-12-11 20:13:54 +0530

    Time: 11 ms (13.48%) | Memory: 37.4 MB (7.15%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<char> st;
-        for(int i = 0;i<s.length();i++){
-            st.push(s[i]);
-        }
-        string a = "";
+        stack<string> st;
         string ans = "";
-        while(!st.empty() && st.top() == ' '){
-            st.pop();
+        int i = 0;
+        string a = "";
+        while(i<s.length()){
+            if((s[i] >= 'a' && s[i] <= 'z')||(s[i] >= 'A' && s[i] <= 'Z') || (s[i]>='0' && s[i] <= '9')){
+                a.push_back(s[i]);
+            }
+            else if(s[i] == ' ' && a.length() > 0){
+                st.push(a);
+                a = "";
+            }
+            i++;
+        }
+        if(a.length() > 0){
+            st.push(a);
         }
         while(!st.empty()){
-            char top = st.top();
+            string top = st.top();
+            ans = ans+top;
             st.pop();
-            if(top == ' ' && a.length() > 0){
-                reverse(a.begin(),a.end());
-                if(ans.length() > 0){
-                    ans = ans + " ";
-                }
-                ans = ans+a;
-                a = "";
-            }else if(top != ' '){
-                a.push_back(top);
+            if(!st.empty()){
+                ans = ans+' ';
             }
-        }
-        if(a.length() >= 1){
-            if(ans.length() > 0){
-                    ans = ans + " ";
-                }
-            reverse(a.begin(),a.end());
-            ans = ans+a;
         }
         return ans;
     }
