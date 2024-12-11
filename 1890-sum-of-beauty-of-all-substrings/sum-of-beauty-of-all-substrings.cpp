dba582ea811eec7e9fde577b144adc1c72bb2e45
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-12-11 20:01:35 +0530

    Time: 125 ms (73.27%) | Memory: 26.1 MB (51.59%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int beautySum(string s) {
         int ans = 0;
-        int n = s.length();
-
-        for (int i = 0; i < n; i++) {
-            vector<int> freq(26, 0); // Frequency array for 'a' to 'z'
-            for (int j = i; j < n; j++) {
-                freq[s[j] - 'a']++; // Update frequency of current character
-
-                int maxi = 0, mini = INT_MAX;
-                for (int f : freq) {
-                    if (f > 0) {
-                        maxi = max(maxi, f);
-                        mini = min(mini, f);
+        for (int i = 0; i < s.length(); i++) {
+            string st = "";
+            unordered_map<char, int> mp;
+            for (int j = i; j < s.length(); j++) {
+                mp[s[j]]++;
+                int maxi = INT_MIN;
+                int mini = INT_MAX;
+                for (auto k : mp) {
+                    if (k.second > maxi) {
+                        maxi = k.second;
+                    }
+                    if (k.second < mini) {
+                        mini = k.second;
                     }
                 }
-                ans += (maxi - mini);
+                ans += maxi-mini;
             }
         }
 
