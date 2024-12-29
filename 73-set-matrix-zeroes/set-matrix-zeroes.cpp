86e9ca6bd0183e944754d2905ea72536e9a3125c
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2024-12-29 12:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (28.93%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<pair<int, int>> arr;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    pair<int, int> p;
+        vector<pair<int,int> > track;
+
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j] == 0){
+                    pair<int,int> p;
                     p.first = i;
                     p.second = j;
-                    arr.push_back(p);
+                    track.push_back(p);
                 }
             }
         }
-        for (int k = 0; k < arr.size(); k++) {
-            pair<int, int> p = arr[k];
-            int i = p.first;
-            int j = p.second;
 
-            // do row 0
-            for (int k = 0; k < cols; k++) {
-                matrix[i][k] = 0;
+        for(int i = 0;i<track.size();i++){
+            pair<int,int> p = track[i];
+            int x = p.first;
+            int y = p.second;
+
+            for(int m = 0;m<matrix.size();m++){
+                matrix[m][y] = 0;
             }
-            // do col 0
-            for (int l = 0; l < rows; l++) {
-                matrix[l][j] = 0;
+            for(int n = 0;n<matrix[0].size();n++){
+                matrix[x][n] = 0;
             }
         }
     }
