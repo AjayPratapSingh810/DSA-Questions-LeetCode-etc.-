c3d0a9e0f44ce3e002cccb08502cfe82fa9024d0
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2025-02-16 15:24:04 +0530

    Time: 25 ms (38.20%) | Memory: 38 MB (48.83%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+
+        vector<vector<bool> > visited(rows,vector<bool> (cols,false));
         queue<pair<int,pair<int,int> > > q;
-        int size = mat.size();
-        int size2 = mat[0].size();
-        vector<vector<int> > ans(size,vector<int> (size2,0));
-        vector<vector<bool> > visited(size,vector<bool> (size2,false));
-        for(int i=0;i<mat.size();i++){
-            for(int j= 0;j<mat[i].size();j++){
+        for(int i = 0;i<rows;i++){
+            for(int j = 0;j<cols;j++){
                 if(mat[i][j] == 0){
-                    q.push(make_pair(0,make_pair(i,j)));
                     visited[i][j] = true;
+                    q.push(make_pair(0,make_pair(i,j)));
+                }else{
+                    mat[i][j] = INT_MAX;
                 }
             }
         }
@@ -18,31 +20,38 @@ public:
         while(!q.empty()){
             pair<int,pair<int,int> > p = q.front();
             q.pop();
-            int x = p.first;
-            int i= p.second.first;
-            int j= p.second.second;
-
-            if(i>0 && !visited[i-1][j]){
-                visited[i-1][j] = true;
-                ans[i-1][j] = x+1;
-                q.push(make_pair(x+1,make_pair(i-1,j)));
+            int price = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+            // up
+            if(row > 0 && mat[row-1][col] != 0){
+                if(mat[row-1][col] > price+1){
+                    mat[row-1][col] = price+1;
+                    q.push(make_pair(price+1,make_pair(row-1,col)));
+                }
             }
-            if(j<mat[i].size()-1 && !visited[i][j+1]){
-                visited[i][j+1] = true;
-                ans[i][j+1] = x+1;
-                q.push(make_pair(x+1,make_pair(i,j+1)));
-            } 
-            if(i<mat.size()-1 && !visited[i+1][j]){
-                visited[i+1][j] = true;
-                ans[i+1][j] = x+1;
-                q.push(make_pair(x+1,make_pair(i+1,j)));
+            // down
+            if(row < rows-1 && mat[row+1][col] != 0){
+                if(mat[row+1][col] > price+1){
+                    mat[row+1][col] = price+1;
+                    q.push(make_pair(price+1,make_pair(row+1,col)));
+                }
             }
-            if(j>0 && !visited[i][j-1]){
-                visited[i][j-1] = true;
-                ans[i][j-1] = x+1;
-                q.push(make_pair(x+1,make_pair(i,j-1)));
+            // left
+            if(col > 0 && mat[row][col-1] != 0){
+                if(mat[row][col-1] > price+1){
+                    mat[row][col-1] = price+1;
+                    q.push(make_pair(price+1,make_pair(row,col-1)));
+                }
+            }
+            // right
+            if(col < cols-1 && mat[row][col+1] != 0){
+                if(mat[row][col+1] > price+1){
+                    mat[row][col+1] = price+1;
+                    q.push(make_pair(price+1,make_pair(row,col+1)));
+                }
             }
         }
-        return ans;
+        return mat;
     }
 };
