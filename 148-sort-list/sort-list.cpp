c442ac229c7a8e751f6c312432d698c203008214
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2025-02-21 18:27:28 +0530

    Time: 372 ms (5.06%) | Memory: 193.1 MB (5.42%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,21 +10,75 @@
  */
 class Solution {
 public:
+    ListNode* merge(ListNode* a,ListNode* b){
+        ListNode* curr1 = a;
+        ListNode* curr2 = b;
+
+        ListNode* ans = NULL;
+        ListNode* curr = NULL;
+
+        while(a!=NULL && b!= NULL){
+            ListNode* x = NULL;
+            if(a->val < b->val){
+                x = new ListNode(a->val);
+                a = a->next;
+            }else{
+                x = new ListNode(b->val);
+                b = b->next;
+            }
+            if(ans == NULL){
+                curr = x;
+                ans = curr;
+            }else{
+                curr->next = x;
+                curr = curr->next;
+            }
+        }
+        while(a!=NULL){
+            ListNode* x = NULL;
+                x = new ListNode(a->val);
+                a = a->next;
+            
+            if(ans == NULL){
+                curr = x;
+                ans = curr;
+            }else{
+                curr->next = x;
+                curr = curr->next;
+            }
+        }
+        while(b!=NULL){
+            ListNode* x = NULL;
+                x = new ListNode(b->val);
+                b = b->next;
+            
+            if(ans == NULL){
+                curr = x;
+                ans = curr;
+            }else{
+                curr->next = x;
+                curr = curr->next;
+            }
+        }
+        return ans;
+    }
     ListNode* sortList(ListNode* head) {
-        vector<int> arr;
-        ListNode* curr = head;
-        while(curr != NULL){
-            arr.push_back(curr->val);
-            curr = curr->next;
+        if(head == NULL || head->next == NULL){
+            return head;
+        }   
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* temp = NULL;
+
+        while(fast != NULL && fast->next != NULL){
+            temp = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        sort(arr.begin(),arr.end());
-        int i = 0;
-        curr = head;
-        while(curr != NULL){
-            curr->val = arr[i++];
-            curr = curr->next;
-        }
-        return head;
-        
+        temp->next = NULL;
+        ListNode* L = sortList(head);
+        ListNode* R = sortList(slow);
+
+        return merge(L,R);
     }
 };
