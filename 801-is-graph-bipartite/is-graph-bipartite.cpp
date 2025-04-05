b487b25107dac2d37ee65cfec852888bf4d0c87e
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2025-04-05 19:51:48 +0530

    Time: 3 ms (16.33%) | Memory: 18.1 MB (10.54%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
-    bool solveBfs(int i,vector<vector<int> > &graph,vector<int> &colors){
-        queue<int> q;
-        q.push(i);
-        colors[i] = 0;
-
+    void bfs(int i,vector<vector<int>>& graph,unordered_map<int,bool> &mp1,unordered_map<int,bool> &mp2,vector<bool> &visited){
+        queue<pair<int,int>> q;
+        q.push(make_pair(i,0));
+        // node,lvl
+        visited[i] = true;
         while(!q.empty()){
-            int top = q.front();
+            pair<int,int> p = q.front();
             q.pop();
-            for(auto x : graph[top]){
-                if(colors[x] == colors[top]){
-                    return true;
-                }
-                if(colors[top] == 0 && colors[x] == -1){
-                    colors[x] = 1;
-                    q.push(x);
-                }else if(colors[top] == 1 && colors[x] == -1){
-                    colors[x] = 0;
-                    q.push(x);
-                }
+            int node = p.first;
+            int lvl = p.second;
+            if(lvl%2 == 0){
+                mp1[node] = true;
+            }else{
+                mp2[node] = true;
             }
 
+            for(int i = 0;i<graph[node].size();i++){
+                if(!visited[graph[node][i]]){
+                    visited[graph[node][i]] = true;
+                    q.push(make_pair(graph[node][i],lvl+1));
+                }
+            }
         }
-        return false;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        int nodes = graph.size();
-        vector<int> colors(nodes,-1);
+        unordered_map<int,bool> mp1;
+        unordered_map<int,bool> mp2;
 
-        for(int i=0;i<nodes;i++){
-            if(colors[i] == -1){
-                bool ans = solveBfs(i,graph,colors);
-                if(ans){
+        
+        int size = graph.size();
+        vector<bool> visited(size,false);
+        for(int i = 0;i<size;i++){
+            if(!visited[i]){
+                bfs(i,graph,mp1,mp2,visited);
+            }
+        }
+        for(int i = 0;i<graph.size();i++){
+            for(int j = 0;j<graph[i].size();j++){
+                if(mp1[i] && mp1[graph[i][j]]){
+                    return false;
+                }
+                if(mp2[i] && mp2[graph[i][j]]){
                     return false;
                 }
             }
