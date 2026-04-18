f284ac59b6663b1d73b7adc2ba0b075cd759c6aa
ajay pratap singh <145218148+AjayPratapSingh810@users.noreply.github.com>
2026-04-18 16:45:43 +0530

    Time: 15 ms (19.71%) | Memory: 38 MB (22.74%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -11,27 +11,29 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root,int target,int &sum,vector<vector<int> > &ans,vector<int> &arr){
+    void solve(TreeNode* root,int target,vector<int> arr,int sum,vector<vector<int>> &ans){
         if(root == NULL){
             return;
         }
-        if(root->left == NULL && root->right == NULL && sum+root->val == target){
-            arr.push_back(root->val);
-            ans.push_back(arr);
-            arr.pop_back();
+        if(root->left == NULL && root->right == NULL){
+            if(sum+root->val == target){
+                arr.push_back(root->val);
+                ans.push_back(arr);
+                arr.pop_back();
+            }
+            return;
         }
+        sum += root->val;
         arr.push_back(root->val);
-        sum = sum+root->val;
-        solve(root->left,target,sum,ans,arr);
-        solve(root->right,target,sum,ans,arr);
-        sum = sum-root->val;
+        solve(root->left,target,arr,sum,ans);
+        solve(root->right,target,arr,sum,ans);
         arr.pop_back();
+        return;
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int> > ans;
+        vector<vector<int>> ans;
         vector<int> arr;
-        int sum = 0;
-        solve(root,targetSum,sum,ans,arr);
+        solve(root,targetSum,arr,0,ans);
         return ans;
     }
 };
